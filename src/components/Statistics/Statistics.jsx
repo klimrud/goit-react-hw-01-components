b07c9ac0da810f54924d0eb8673c756dc0b43c1f
Tetiana Klimenko <ktlimenko@gmail.com>
2023-03-20 14:08:55 +0200

    statistics-title

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,12 +1,16 @@
+import PropTypes from 'prop-types';
  import { StatsStatistics } from "./StatsStatistics";
  import css from "components/Statistics/Statistics.module.css"
 
-export const Statistics = ({stats}) => {
+export const Statistics = ({stats, title ='Upload stats'}) => {
   return (
   <section className={css.statistics}>
-     <h2 className={css.title}>Upload stats</h2>
+     <h2 className={css.title}>{title}</h2>
      <StatsStatistics stats={stats} />
   </section>
   );
 };
 
+Statistics.propTypes = {
+  title: PropTypes.string,
+}
